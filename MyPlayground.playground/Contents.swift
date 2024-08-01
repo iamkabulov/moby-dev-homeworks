fea9bf3f4aac64cb8da034468f22a355e0f4c3fc
kabulov <n.kabulov@cft.ru>
2024-08-01 21:05:44 +0500

    Сделал Lab 2 (var, let, data types)

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,29 @@
 ///Теперь выведите свои собственные фразы в консоль. Например, выберите вашу любимую песню и имя исполнителя.
 print("Akpen birge")
 print("Moldanazar")
+
+var friends = 51
+friends = friends - 1
+
+let goalSteps = 10000
+
+var schooling = 11
+print(schooling)
+schooling += 1
+print(schooling)
+print("Потому что константа она не меняется, а значение переменной можно переопределять")
+
+var stepsCount = 0
+print(stepsCount)
+stepsCount += 2000
+print(stepsCount)
+print("Хорошая работа! Вы уже на пути к своей ежедневной цели")
+
+
+var name: String
+name = "Name"
+print(name)
+print("Потому что мы хотим использовать значение переменной не объявив его")
+
+var distanceTraveled: Double = 0
+distanceTraveled = 54.3
