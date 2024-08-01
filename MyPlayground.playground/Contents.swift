622f6151ad8ef2c7fa820474fdffa70e527309b9
kabulov <n.kabulov@cft.ru>
2024-08-01 21:31:39 +0500

    Сделал Lab 3 (operators)

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -27,3 +27,44 @@ print("Потому что мы хотим использовать значен
 
 var distanceTraveled: Double = 0
 distanceTraveled = 54.3
+
+//Lab 3
+let width = 10
+let height = 20
+let area = width * height
+let roomArea = area / 2
+print(roomArea)
+let perimeter = 2 * (width + height)
+print(perimeter)
+
+print(12%5)
+
+let heartRate1 = 60
+let heartRate2 = 70
+let heartRate3 = 100
+let addedHR = heartRate1 + heartRate2 + heartRate3
+let addedHRAverage =  addedHR / 3
+print(addedHRAverage)
+let heartRate1D: Double = 60
+let heartRate2D: Double = 70
+let heartRate3D: Double = 100
+let addedHRD = heartRate1D + heartRate2D + heartRate3D
+let addedHRAverageD =  addedHRD / 3
+print(addedHRAverageD)
+print("Такое возможно если среднее значение Int будет без остатка. Потому что Int, выкидывается дробное значение.")
+
+let steps = 3467.0
+let goal = 10000.0
+let percentOfGoal = (steps / goal) * 100
+
+var balance = 0
+balance += 10_000
+balance += 20_000
+balance -= balance/2
+balance *= 3
+balance -= 3000
+print(balance)
+
+print(10 + 2 * 5)
+print((10 + 2) * 5)
+print(4 * (9 - (6 / 2)))
